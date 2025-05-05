760e6ae754064f1c309ab6a800e5467a548ab770
Egor Shesterkin <ewill1989@yandex.ru>
2025-05-05 20:48:26 +0300

    fixes after review

--- scripts/index.js
+++ scripts/index.js
@@ -3,48 +3,32 @@ const cardTemplate = document.querySelector('#card-template').content;
 
 // @todo: DOM узлы
 const placesList = document.querySelector('.places__list');
-const popupNewCard = document.querySelector('.popup_type_new-card');
-const addButton = document.querySelector('.profile__add-button');
-const closeButton = popupNewCard.querySelector('.popup__close');
-const saveButton = popupNewCard.querySelector('.popup__button');
 
 // @todo: Функция создания карточки
 
-function addCard(cardName, cardImage) {
+function addCard(card, removeCard) {
   const cardElement = cardTemplate.querySelector('.card').cloneNode(true);
   const deleteButton = cardElement.querySelector('.card__delete-button');
-  cardElement.querySelector('.card__image').src = cardImage;
-  cardElement.querySelector('.card__image').alt = cardName;
-  cardElement.querySelector('.card__title').textContent = cardName;
-  placesList.append(cardElement);
+  const cardImage = cardElement.querySelector('.card__image');
+
+  cardImage.src = card.link; 
+  cardImage.alt = card.name;
+  cardElement.querySelector('.card__title').textContent = card.name;
 
   deleteButton.addEventListener('click', function () {
-    removeCard(deleteButton);
+    removeCard(cardElement);
   });
+
+  return cardElement;
 };
 
 // @todo: Функция удаления карточки
-function removeCard(deleteButton) {
-  const listItem = deleteButton.closest('.card');
-  listItem.remove();
+function removeCard(card) {
+  card.remove();
 };
 
 // @todo: Вывести карточки на страницу
 
-initialCards.forEach((item) => {  
-  addCard(item.name, item.link);
-});
-
-addButton.addEventListener('click', function () {
-  popupNewCard.classList.add('popup_is-opened');
-});
-
-closeButton.addEventListener('click', function () {
-  popupNewCard.classList.remove('popup_is-opened');
-});
-
-saveButton.addEventListener('click', function () {
-  const inputCardName = popupNewCard.querySelector('.popup__input_type_card-name');
-  const inputCardImage = popupNewCard.querySelector('.popup__input_type_url');
-  addCard(inputCardName.value, inputCardImage.value);
+initialCards.forEach((card) => { 
+  placesList.append(addCard(card, removeCard));
 });
